bec2eaac77d8a7cc5aa7caa773b9fa58538073dc
r-sie <32829606+Arsiela@users.noreply.github.com>
2024-09-19 17:11:37 +0800

    09192024 - Update Vehicle Sales Invoice
    09192024 - Update Vehicle Sales Invoice

--- src/main/java/org/guanzon/auto/validator/cashiering/Validator_VehicleSalesInvoice_Source.java
+++ src/main/java/org/guanzon/auto/validator/cashiering/Validator_VehicleSalesInvoice_Source.java
@@ -5,7 +5,16 @@
  */
 package org.guanzon.auto.validator.cashiering;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.guanzon.appdriver.base.GRider;
+import org.guanzon.appdriver.base.MiscUtil;
+import org.guanzon.appdriver.base.SQLUtil;
+import org.guanzon.appdriver.constant.TransactionStatus;
 import org.guanzon.auto.model.cashiering.Model_VehicleSalesInvoice;
 
 /**
@@ -50,12 +59,62 @@ public class Validator_VehicleSalesInvoice_Source implements ValidatorInterface 
             }
         }
         
+        
+        try {
+            //check if linked with PR
+            String lsID = "";
+            String lsDesc = "";
+            String lsType = "";
+            String lsSQL =   " SELECT "                                             
+                    + "   a.sTransNox "                                     
+                    + " , a.sReferNox "                                     
+                    + " , a.sSourceCD "                                     
+                    + " , a.sSourceNo "                                     
+                    + " , a.sTranType "                                     
+                    + " , b.sReferNox AS sSINoxxxx "                        
+                    + " , b.dTransact "                      
+                    + " , b.cTranStat  "                                  
+                    + " FROM si_master_source a "                           
+                    + " LEFT JOIN si_master b ON b.sTransNox = a.sTransNox ";
+            lsSQL = MiscUtil.addCondition(lsSQL, " b.cTranStat <> " + SQLUtil.toSQL(TransactionStatus.STATE_CANCELLED) 
+                                                + " AND a.sReferNox = " + SQLUtil.toSQL(poEntity.getReferNo()) 
+                                                );
+            System.out.println("EXISTING VSI CHECK: " + lsSQL);
+            ResultSet loRS = poGRider.executeQuery(lsSQL);
+
+            if (MiscUtil.RecordCount(loRS) > 0){
+                    while(loRS.next()){
+                        lsID = loRS.getString("sSINoxxxx");
+                        lsType = loRS.getString("sTranType"); 
+                        lsDesc = xsDateShort(loRS.getDate("dTransact"));
+                    }
+
+                    MiscUtil.close(loRS);
+
+                    psMessage = "Found an existing vehicle sales invoice."
+                            + "\n\n<Invoice No:" + lsID + ">"
+                            + "\n<Invoice Date:" + lsDesc + ">"
+                            + "\n<Invoice Type:" + lsType + ">"
+                            + "\n\nCancellation aborted.";
+                    return false;
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Validator_VehicleSalesInvoice_Source.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         return true;
     }
-
+    
     @Override
     public String getMessage() {
         return psMessage;
+    }
+    
+    private static String xsDateShort(Date fdValue) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String date = sdf.format(fdValue);
+        return date;
     }
     
 }
